b4f1d3bf560edec3e16d82e0a16c616d937e7bb7
Botii <45981726+Botii@users.noreply.github.com>
2020-12-08 10:29:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const player1Spikes = [];
 
+MENU = 0;
+
+
 
 
 let astranoutImg;
@@ -9,14 +12,21 @@ let song;
 let timer = 20;
 let countDownSwitch = false;
 
-
-let scorePlayer1 = 0;
+let scorePlayer1 = 0;   
 let player1winning = false;
 
 
 
+showingScore = 0 
+
+
 // GAME WIDTH AND HEIGHT
 const gameWH = 500;
+
+
+    
+
+
 
 function preload() {
   astranoutImg = loadImage("assets/astranout.png");
@@ -31,16 +41,19 @@ function setup() {
 
   imageMode(CENTER, CENTER);
 
+
+
+
   astranout1 = createSprite();
   astranout1.scale = 0.1;
   astranout1.addImage(astranoutImg);
   astranout1.position.y = gameWH * 0.9;
   astranout1.position.x = gameWH * 0.1;
 
+  let facts = ['The Apollo astronauts\nfootprints on\nthe moon will probably stay there\nfor at least 100 million years', 'Space is completely silent', ' 99 percent of our solar\nsystems mass is the sun', 'One day on Venus is longer\nthan one year on Earth'];
+  Fact = random(facts)
 
-
-
-
+  
 
   // GENERATE SPIKES FOR PLAYER 1
   for (let x = 0; x < 3; x++) {
@@ -53,9 +66,31 @@ function setup() {
 }
 
 
-function draw() {
-  background(50);
 
+
+function draw() {
+  console.log(mouseX, mouseY);
+  console.log(MENU);
+
+  background(255);
+
+  fill(0, 255, 0);
+  rect(50, 50, 200, 75);
+  fill(255, 0, 0);
+  rect(50, 200, 200, 75);
+  fill(255, 127, 0);
+  rect(50, 350, 200, 75);
+  textSize(50)
+  fill(255);
+  text('START', 70, 106);
+  text('EXIT', 94, 406);
+  textSize(26);
+  text('INSTRUCTIONS', 52, 248);
+
+
+  if (MENU === 1){
+
+   background(50);
 
     drawSprites();
     playerControls();
@@ -66,7 +101,7 @@ function draw() {
     textSize(19.5);
     text(scorePlayer1, 80, 22);
   
-  
+
     /////////////timer////////////////
     textAlign(CENTER,CENTER);
     textSize(100);
@@ -77,16 +112,74 @@ function draw() {
     if (timer <= 0){
       textSize(20);
       text('game over', width/2, height * 0.667);
-      GameOver();
-  
-
+      MENU = 0
+      showingScore =+ scorePlayer1;
   }
+  }
+
+
+  if (MENU === 0){
+
+    timer = 20; 
+    scorePlayer1 = 0
+
+
+
+
+    background(50);
+
+    textSize(20)
+    text("YOUR SCORE = " + showingScore, width / 2 + 20, height / 2 - 100)
+
+
+
+
+    fill(0, 255, 0);
+    rect(50, 50, 200, 75);
+    fill(255, 0, 255);
+    rect(50, 200, 200, 75);
+    fill(255, 0, 0);
+    rect(50, 350, 200, 75);
+    textSize(50)
+    fill(255);
+    text('START', 70, 106);
+    text('EXIT', 94, 406);
+    textSize(26);
+    text('INSTRUCTIONS', 52, 248);
+    textSize(15)
+    text("FUN FACT = \n" + Fact, width / 2 +20, height / 2 + 150)
+
+    
+
+    
+  }
+
+  if (MENU == 2) {
+    background(50)
+    textSize(20)
+    text('Right Click to return to MENU', 200, 100)
+    textSize(10)
+    text('1. Rockets will fall from the top of the screen.', 50, 150)
+    text('2. Move your character using arrow keys', 50, 200)
+    text('<- and -> to catch the rockets, every rocket will increase your score', 80, 240)
+    text('3. The game is over when the timer runs out, go CATCH SOME ROCKETS.', 50, 290)
+    
+    if (mouseButton == RIGHT) {
+      MENU = 0
+    }
+  }
+  if (MENU === 3){
+    background(255, 0, 0)
+    textSize(45)
+    text('COME AGAIN SOON!', 25, height / 2)
+    
+  }
+
  
 }
 
 
 function spikesDown() {
-
 
 
       // PLAYER 1 SPIKES
@@ -127,8 +220,6 @@ function spikesDown() {
 
     }
   }
-  
-
 }
 
   
@@ -147,18 +238,94 @@ function playerControls() {
       astranout1.position.x += 10;
     }
   }
-
 }
 
 
-function GameOver(Timer, gameOverScreen){
-  background(50)
-  rect(0,0, 500, 500)
-  textAlign(CENTER);
-  text('GAME OVER', width / 2, height / 2)
-  text("SCORE = " + scorePlayer1, width / 2, height / 2 + 20)
-  text('click to play again', width / 2, height / 2 + 40);
-  Erase();
+
+function mouseClicked() {
+  if (MENU == 0) {
+    if (mouseX < 200 && mouseX > 50) {
+      if (mouseY < 125 && mouseY > 50) {
+        MENU = 1
+      }
+      if (mouseY < 275 && mouseY > 200) {
+        MENU = 2
+      }
+      if (mouseY < 425 && mouseY > 350) {
+        MENU = 3
+      }
+    }
+  }
 }
+
+
+
+
+
+
+
+//function GameOver(Timer, gameOverScreen){
+ // background(50)
+  //rect(0,0, 500, 500)
+  //textAlign(CENTER);
+  //text('GAME OVER', width / 2, height / 2)
+  //text("SCORE = " + scorePlayer1, width / 2, height / 2 + 20)
+  //text('click to play again', width / 2, height / 2 + 40);
+  //Erase();
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
